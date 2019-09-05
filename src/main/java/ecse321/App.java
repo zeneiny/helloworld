64a80d34affd9dc47b8a0747000e3f3ebd9b3a01
Shane McIntosh <shane.mcintosh@mcgill.ca>
2019-09-05 09:47:12 -0400

    Woo Revert "Translation to french"
    This reverts commit 090a10c096d79fc735265a53887b57fc5cc55101.

--- src/main/java/ecse321/App.java
+++ src/main/java/ecse321/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour tout le monde!" );
+        System.out.println( "Hello World!" );
     }
 }
