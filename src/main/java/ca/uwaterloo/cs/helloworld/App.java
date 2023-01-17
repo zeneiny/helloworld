c778efaa419ac84f9f7beed072738cb443c9c746
Shane McIntosh <shanemcintosh@acm.org>
2023-01-17 15:13:25 -0500

    Reset repository for exercise

--- src/main/java/ca/uwaterloo/cs/helloworld/App.java
+++ src/main/java/ca/uwaterloo/cs/helloworld/App.java
@@ -7,7 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour tout le monde" );
-        System.out.println( "CS 489 is awesome!" );
+        System.out.println( "Hello, world!" );
     }
 }
